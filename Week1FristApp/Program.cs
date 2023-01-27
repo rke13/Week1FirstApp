c8a65919eb9f676fc55947824cd979bc3f7a98be
Julia Vrn <example@mail.com>
2023-01-27 11:43:39 +0100

    add my first app

---
+++ Week1FristApp/Program.cs
@@ -0,0 +1,8 @@
+﻿//This is a comment
+
+string myName = "Harry";
+string myLastName = "Potter";
+
+//Console.WriteLine("Hello" + " " + myName);
+
+Console.WriteLine($"Hello, {myName} {myLastName}!");
